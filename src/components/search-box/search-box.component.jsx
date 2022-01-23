8c5f9b3e4d46f62abf433eff74831d828c7237f2
Supriya <supriyabarai77@gmail.com>
2022-01-23 23:40:52 +0530

    Initial commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <input type='search' className='search' 
+        placeholder={ placeholder } 
+        onChange={ handleChange }
+    />
+);
